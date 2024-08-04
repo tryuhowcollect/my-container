ccc3950fda7f2c2bb7ffee195dfe9ea3c0f0fe5e
tryuhowcollect <t.ryu.howcollect@gmail.com>
2024-08-04 14:25:06 +0900

    feat: network

--- src/main.go
+++ src/main.go
@@ -21,18 +21,13 @@ func main() {
 	}
 }
 
-// child()関数が新しい名前空間内で実行され、ホストシステムと隔離された環境で/bin/bash シェルを実行できる
 func run() {
 	fmt.Printf("Running %v\n", os.Args[2:])
-	// exec.Commandで新しいプロセスを作成(/simple_container child /bin/bash)
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
-	// 新しいプロセスの標準入力、標準出力、標準エラー出力を現在のプロセスに接続
 	cmd.Stdin, cmd.Stdout, cmd.Stderr = os.Stdin, os.Stdout, os.Stderr
-	// 4つのnamespaceを作成(Mount, UTS, PID, NET)
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWNET,
 	}
-	// 3つのnamespaceの中で新しいプロセスを実行
 	if err := cmd.Run(); err != nil {
 		fmt.Printf("Error running command: %v\n", err)
 	}
@@ -40,22 +35,82 @@ func run() {
 
 func child() {
 	fmt.Printf("Running %v\n", os.Args[2:])
-	// NEWPIDに対応
-	cmd := exec.Command(os.Args[2], os.Args[3:]...) // /bin/bash
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin, cmd.Stdout, cmd.Stderr = os.Stdin, os.Stdout, os.Stderr
-	// 新しいUTS名前空間内でホスト名を "container" に設定(独自のホスト名)
-	syscall.Sethostname([]byte("container"))
 
-	// 新しいルートディレクトリを設定
-	//syscall.Chroot("/")
-	// 現在の作業ディレクトリを新しいルートに変更(上記で指定したディレクトリからの相対パス)
-	//os.Chdir("/")
-	// proc ファイルシステムを /proc にマウント
-	syscall.Mount("proc", "/proc", "proc", 0, "")
+	// ホスト名を "container" に設定
+	if err := syscall.Sethostname([]byte("container")); err != nil {
+		fmt.Printf("Error setting hostname: %v\n", err)
+		return
+	}
 
+	// proc ファイルシステムをマウント
+	if err := syscall.Mount("proc", "/proc", "proc", 0, ""); err != nil {
+		fmt.Printf("Error mounting proc: %v\n", err)
+		return
+	}
+
+	// Vethペアを作成してIPアドレスを設定
+	if err := setupVeth(); err != nil {
+		fmt.Printf("Error setting up veth: %v\n", err)
+		return
+	}
+
+	// コマンドを実行
 	if err := cmd.Run(); err != nil {
 		fmt.Printf("Error running command: %v\n", err)
 	}
-	// プロセス終了後に proc ファイルシステムをアンマウント
-	syscall.Unmount("/proc", 0)
+
+	// proc ファイルシステムをアンマウント
+	if err := syscall.Unmount("/proc", 0); err != nil {
+		fmt.Printf("Error unmounting proc: %v\n", err)
+	}
+}
+
+func setupVeth() error {
+	// Vethペアを作成
+	if err := exec.Command("ip", "link", "add", "veth0-host", "type", "veth", "peer", "name", "veth0-ct").Run(); err != nil {
+		return fmt.Errorf("failed to create veth pair: %v", err)
+	}
+
+	// VethペアにIPアドレスを設定
+	if err := exec.Command("ip", "addr", "add", "10.10.10.10/24", "dev", "veth0-host").Run(); err != nil {
+		return fmt.Errorf("failed to assign IP to veth0-host: %v", err)
+	}
+	if err := exec.Command("ip", "link", "set", "up", "dev", "veth0-host").Run(); err != nil {
+		return fmt.Errorf("failed to bring up veth0-host: %v", err)
+	}
+
+	// コンテナ側のVethインターフェースを新しいネットワーク名前空間に移動
+	if err := moveToNetNS("veth0-ct"); err != nil {
+		return fmt.Errorf("failed to move veth0-ct to netns: %v", err)
+	}
+
+	// 新しいネットワーク名前空間内での設定
+	if err := exec.Command("ip", "netns", "exec", "netns01", "ip", "addr", "add", "10.10.10.11/24", "dev", "veth0-ct").Run(); err != nil {
+		return fmt.Errorf("failed to assign IP to veth0-ct in netns: %v", err)
+	}
+	if err := exec.Command("ip", "netns", "exec", "netns01", "ip", "link", "set", "up", "dev", "veth0-ct").Run(); err != nil {
+		return fmt.Errorf("failed to bring up veth0-ct in netns: %v", err)
+	}
+
+	return nil
+}
+
+func moveToNetNS(ifName string) error {
+	fmt.Printf("Moving %s to new netns\n", ifName)
+
+	// 新しいネットワーク名前空間を作成
+	if err := exec.Command("ip", "netns", "add", "netns01").Run(); err != nil {
+		return fmt.Errorf("failed to create netns: %v", err)
+	}
+
+	// インターフェースを新しいネットワーク名前空間に移動
+	cmd := exec.Command("ip", "link", "set", ifName, "netns", "netns01")
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("Error moving %s to netns: %v: %s", ifName, err, string(output))
+	}
+
+	return nil
 }
